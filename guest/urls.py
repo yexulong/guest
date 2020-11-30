e0aa14686bece2b2d3ddc240e6ea177ac5270a7b
wb.yexulong <wb.yexulong@mesg.corp.netease.com>
2020-11-30 17:11:10 +0800

    modify urls

--- guest/urls.py
+++ guest/urls.py
@@ -13,13 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls import url
+from django.views import static
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
 from sign import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^static/(?P<path>.*)$', static.serve,
+        {'document_root': settings.STATIC_ROOT}, name='static'),
 
     # sign应用
     # url(r'^index/$', views.index),
